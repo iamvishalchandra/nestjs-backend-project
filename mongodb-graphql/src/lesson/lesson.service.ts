1400cd34645a74f086cd9c124c0ad601aeff3259
iamvishalchandra <vishal.chandra75@gmail.com>
2021-08-16 22:52:34 +0530

    graphql mongodb

--- mongodb-graphql/src/lesson/lesson.service.ts
+++ mongodb-graphql/src/lesson/lesson.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Lesson } from './lesson.entity';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class LessonService {
@@ -9,11 +10,14 @@ export class LessonService {
     @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
   ) {}
 
-  async createLesson(name, startDate, endDate) {
+  async createLesson(name, startDate, endDate): Promise<Lesson> {
     const lesson = this.lessonRepository.create({
+      id: uuid(),
       name,
       startDate,
       endDate,
     });
+
+    return this.lessonRepository.save(lesson);
   }
 }
